255d9e3d03c92197d61c59aaf436f9b6a01a48d2
Greg <gregory.voloir@epitech.eu>
2020-01-15 15:40:11 +0100

    [CI]
    ------
    [MOD]:
    Changing ci, passes from circleci to travisci.

--- .circleci/conan.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-conan remote add conan-centers https://conan.bintray.com
-conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
-
-conan profile new default --detect
-conan profile update settings.compiler.libcxx=libstdc++11 default
