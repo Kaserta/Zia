f3cccde34f3d418f5ff59f871c781c099609d727
Greg <gregory.voloir@epitech.eu>
2020-01-17 22:16:17 +0100

    [Travis] ----- [MOD] travis yaml modified to make all the things work.
    -----
    [ADD]
    Some dummy code to test everything.

--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include "gtest/gtest.h"
 
-int main(int ac, char **av)
-{
-	std::cout << "hello zia !" << std::endl;
-	return 17;
+int ireturntwo() {
+	return 2;
+}
+
+TEST(ireturntwo, returning_two) {
+	EXPECT_EQ (2, ireturntwo());
+}
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
