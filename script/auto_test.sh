0bd1daabe45e4645609c7174df3f87ef9d589abc
Greg <gregory.voloir@epitech.eu>
2020-01-20 20:17:50 +0100

    [Travis] ----- [MOD] Finnally.

---
+++ script/auto_test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+conan remote add conan-centers https://conan.bintray.com
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+
+conan profile new default --detect
+conan profile update settings.compiler.libcxx=libstdc++11 default
+
+#compile
+mkdir -p build
+cd build
+conan install .. --build=missing
+cmake .. -G "Unix Makefiles"
+make all test
+cd ..
+gcov  $(find build/test/CMakeFiles/Glados.dir -iname "*.cpp.o")
+gcovr -g -e test
