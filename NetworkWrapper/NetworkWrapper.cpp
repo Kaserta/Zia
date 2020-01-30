401bde6dfcc904fe1837f18471d94a4b8f5276a1
florian <florian@localhost.localdomain>
2020-01-30 17:37:00 +0100

    [ADD] add start of network manager with wrapping of C socket with good heriting way

---
+++ NetworkWrapper/NetworkWrapper.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2020
+** NetworkWrapper
+** File description:
+** network wrapper for c++17 socket
+*/
+
+#include "NetworkWrapper.hpp"
+
+NetworkWrapper::NetworkWrapper()
+{
+}
+
+NetworkWrapper::~NetworkWrapper()
+{
+}
