714cd35502302c47598c49e122acede577e7c5e2
florian <florian@localhost.localdomain>
2020-02-04 16:47:27 +0100

    [NetworkWrapper]
    ----------------
    
    [MOD] put a ternary for socket creation instead of else if
    
    [ADD] add some definitions in cmake list for windows and linux

--- zia/NetworkWrapper/src/Socket.cpp
+++ zia/NetworkWrapper/src/Socket.cpp
@@ -42,10 +42,7 @@ Socket::Socket(sockType protocol, bool isV6)
 
     if (isV6)
         ipVersionType = AF_INET6;
-    if (protocol == TCP)
-        m_socketFd = socket(ipVersionType, SOCK_STREAM, IPPROTO_TCP);
-    else
-        m_socketFd = socket(ipVersionType, SOCK_DGRAM, IPPROTO_UDP);
+    m_socketFd = TCP ? socket(ipVersionType, SOCK_STREAM, IPPROTO_TCP) : socket(ipVersionType, SOCK_DGRAM, IPPROTO_UDP);
     if (m_socketFd <= 0)
         throw Zia::Exceptions::NetworkException("Socket could not be created");
 }
