e59e1a7ae4b7172ec540e42faa468abad8cc8cb6
florian <florian.lavabre@epitech.eu>
2020-02-12 11:54:05 +0100

    [NetworkWrapper]
    -------------
    
    [ADD] add main to test network wrapping on windows
    
    [MOD] modified cmakelist to put debug with g3 on unix
    
    [MOD] modified zia/cmakelists to add target link libraries

--- zia/main.cpp
+++ zia/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include "NetworkWrapper/include/Socket.hpp"
+#include "NetworkWrapper/include/PollWrap.hpp"
+#include <sys/socket.h>
+#include <memory>
+#include <unistd.h>
+
+void salut(std::shared_ptr<Zia::Network::Socket> socket, short lebikini)
+{
+    std::cout << "salut je suis dans salut: mon short est :" << lebikini <<std::endl;
+    std::shared_ptr<Zia::Network::Socket> i = socket->accept();
+    std::cout << "le getfd de l'accept de la socket :" << i->getFD() << std::endl;
+    std::cout << socket->getFD() << std::endl;
+
+}
 
 int main()
 {
     std::cout << "Hello World!" << std::endl;
-	while (1);
+    std::shared_ptr<Zia::Network::Socket> p = std::make_shared<Zia::Network::Socket>(Zia::Network::sockType::TCP);
+    p->bind(4242);
+    p->listen(100);
+    Zia::Network::PollWrap u;
+    u.addToWatch(p, &salut, POLLIN);
+    u.start();
 }
