b5ef11e954bbd4c4bc13066c305a3dfed946394e
tralalalolo <45788026+tralalalolo@users.noreply.github.com>
2020-02-17 11:21:21 +0100

    [FIX]: changing sleep with sleep_for which is portable
    Sleep does not exist, and sleep MAY suspend the current process (on linux), and suspend the current thread on windows.
    By default, prefre sleep_for, which will suspen the thread

--- zia/main.cpp
+++ zia/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include "Log.hpp"
+#include <thread>
 
 int main()
 {
     std::cout << "I'm a program !" << std::endl;
     WARNLOG("I'm a warning bruh.");
-    Sleep(5000);
+    std::this_thread::sleep_for(std::chrono::seconds(5));
     INFOSLOG("I'm an infos 5 sec after the warning.");
-    Sleep(5000);
+    std::this_thread::sleep_for(std::chrono::seconds(5));
     ERRLOG("And I'm the final error 5 sec after the infos.");
 }
